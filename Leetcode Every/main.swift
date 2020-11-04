21e09cd1e5af253c94eeb7c5e7396ad5610a5870
Weixi779 <1349750521@qq.com>
2020-11-04 10:59:38 +0800

    Version v1.0

--- Leetcode Every/main.swift
+++ Leetcode Every/main.swift
@@ -6,6 +6,28 @@
 //
 
 import Foundation
+//-MARK: Leetcode 每日一题 201011
+//var test201011 = Solution201011()
+////print(test201011.canPartition([1,5,10,1,5]))
+//
+//var test201014 = Solution201014()
+//
+//var test201016 = Solution201016()
+////print(test201016.sortedSquares([-4,-1,0,3,10]))
 
-print("Hello, World!")
+var test201025 = Solution201025()
+print(test201025.longestMountain([2,1,4,7,3,2,5]))
+//var test201026 = Solution201026()
+////print(test201026.smallerNumbersThanCurrent([8,1,2,2,3]))
+//var test201028 = Solution201028()
+////print(test201028.uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0]))
+//var test201030 = Solution201030()
+////print(test201030.islandPerimeter([[1,0]]))
+//var text201102 = Solution201102()
+////print(text201102.intersection([4,9,5], [9,4,9,8,4]))
+//var text201103 = Solution201103()
+//print(text201103.validMountainArray([0,1,2,3,4,5,6,7,8,9]))
+
+var test201104 = Solution201104()
+print(test201104.insert2([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))
 

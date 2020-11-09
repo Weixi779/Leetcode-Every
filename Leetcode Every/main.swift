2a39bbf1aa5b70f7a5e2995676fcc871d14ddd53
Weixi779 <1349750521@qq.com>
2020-11-09 09:01:29 +0800

    Version v1,1~

--- Leetcode Every/main.swift
+++ Leetcode Every/main.swift
@@ -28,6 +28,19 @@ print(test201025.longestMountain([2,1,4,7,3,2,5]))
 //var text201103 = Solution201103()
 //print(text201103.validMountainArray([0,1,2,3,4,5,6,7,8,9]))
 
-var test201104 = Solution201104()
-print(test201104.insert2([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))
+//var test201104 = Solution201104()
+//print(test201104.insert2([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))
 
+//var test201105 = Solution201105()
+//print(test201105.ladderLength("cat", "fin", ["ion","rev","che","ind","lie","wis","oct","ham","jag","ray","nun","ref","wig","jul","ken","mit","eel","paw","per","ola","pat","old","maj","ell","irk","ivy","beg","fan","rap","sun","yak","sat","fit","tom","fin","bug","can","hes","col","pep","tug","ump","arc","fee","lee","ohs","eli","nay","raw","lot","mat","egg","cat","pol","fat","joe","pis","dot","jaw","hat","roe","ada","mac"]))
+
+
+//var test201106 = Solution201106()
+//print(test201106.sortByBits([1024,512,256,128,64,32,16,8,4,2,1]))
+//var test201107 = Solution201107()
+//print(test201107.reverse(-123))
+var test201108 = Solution201108()
+print(test201108.maxProfit([7,1,5,3,6,4]))
+
+var test201109 = Solution201109()
+print(test201109.kClosest([[3,3],[5,-1],[-2,4]], 2))
